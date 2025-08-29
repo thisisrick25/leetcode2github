bb20a443a1f13c3c6bf82b76560fb3995d908c62
thisisrick25 <thisisrick25@users.noreply.github.com>
2025-08-29 20:12:59 +0000

    feat(3375): add cpp solution for 3375-minimum-operations-to-make-array-values-equal-to-k [skip ci]

---
+++ leetcode-solutions/3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+
+        int minValue = *min_element(nums.begin(), nums.end());
+        
+        if (minValue < k) {
+            return -1;
+        }
+
+        int opCount = 0;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > k) {
+                ++opCount;
+
+                while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+                    i++;
+                }
+            }
+        }
+
+        return opCount;
+    }
+};
