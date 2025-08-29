ac1beb1342087f8f13de5c780e79c66816ff00be
thisisrick25 <thisisrick25@users.noreply.github.com>
2025-08-29 20:18:19 +0000

    feat(0446): add cpp solution for 0446-arithmetic-slices-ii-subsequence [skip ci]

---
+++ leetcode-solutions/0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        
+        long n = nums.size();
+        long ans = 0;
+        vector<unordered_map<long, long>> dp(n); // dp[i][d]
+        for (long i = 1; i < n; i++) {
+            for (long j = 0; j < i; j++) {
+                long diff = (long)nums[i] - (long)nums[j];
+                
+                long cnt = dp[j].count(diff) ? dp[j][diff] : 0;
+                dp[i][diff] += cnt + 1;
+                ans += cnt;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
