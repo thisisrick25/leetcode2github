ecd2d76d56b3fea44c8b5069d86dfbcf99013fba
thisisrick25 <thisisrick25@users.noreply.github.com>
2025-11-20 19:23:09 +0000

    feat(0713): add cpp solution for 0713-subarray-product-less-than-k [skip ci]

---
+++ leetcode-solutions/0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        
+        int begin = 0, end = 0, prod = 1, ans = 0;
+        
+        if(k <= 1)
+            return 0;
+        
+        while(end < nums.size()) {
+            prod *= nums[end];
+            end++;
+            
+            while(prod >= k) {
+                prod /= nums[begin];
+                begin++;
+            }
+            ans += end - begin;
+            
+        }
+        return ans;
+    }
+};
