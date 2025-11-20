fc2bf13a7949b77fc91e76e27f88e035bde3f162
thisisrick25 <thisisrick25@users.noreply.github.com>
2025-11-20 18:18:52 +0000

    feat(2022): add cpp solution for 2022-convert-1d-array-into-2d-array [skip ci]

---
+++ leetcode-solutions/2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        
+        if(original.size() != m*n)
+            return {};
+        
+        vector<vector<int>> ans(m, vector<int>(n));
+        int index = 0;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                ans[i][j] = original[index];
+                index++;
+            }
+        }
+        
+//         for(int i = 0; i < original.size(); i++) {
+//             ans[i / n][i % n] = original[i];
+//         }
+        
+        return ans;
+        
+    }
+};
