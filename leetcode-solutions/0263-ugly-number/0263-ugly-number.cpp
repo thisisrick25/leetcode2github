598ec95b1c306b6909a7c8a2626c6c6acf982ecc
thisisrick25 <thisisrick25@users.noreply.github.com>
2025-11-20 19:26:06 +0000

    feat(0263): add cpp solution for 0263-ugly-number [skip ci]

---
+++ leetcode-solutions/0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+//     vector<int> primes = {2, 3, 5};
+//     bool isUgly(int n) {
+//         if (n < 1) 
+//             return false;
+//         for (int p: primes) {
+            
+//             while (n % p == 0) 
+//                 n /= p;
+//         }
+        
+//         if(n==1)
+//             return true;
+        
+//         return false;
+//     }
+    
+    
+    bool isUgly(int n) {
+        if(n==0)
+            return false;
+        while(n%2==0)
+            n/=2;
+        while(n%3==0)
+            n/=3;
+        while(n%5==0)
+            n/=5;
+        if(n==1)
+            return true;
+        return false;
+    }
+    
+    
+    
+//     bool isUgly(int num) {
+        
+//         if(num<1)
+//             return false;
+//         else if(num==1)
+//             return true;
+//         else if(num%2==0)
+//             return isUgly(num/2);
+//         else if(num%3==0)
+//             return isUgly(num/3);
+//         else if(num%5==0)
+//             return isUgly(num/5);
+//         else
+//             return false;
+        
+//     }
+};
